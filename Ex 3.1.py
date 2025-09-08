565500b2b1bd7517b073dc38cb10e65f957c91a9
Priyanka Lakshmanan <47346754+priyankalakshmanan@users.noreply.github.com>
2025-09-08 16:31:20 -0400

    Update Ex 3.1.py
    Conditional statements exercise

--- Ex 3.1.py
+++ Ex 3.1.py
@@ -1,6 +1,7 @@
 #3.1 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
 #Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. 
 
+
 hrs = input("Enter Hours:")
 rate = input("Enter rate:")
 try:
